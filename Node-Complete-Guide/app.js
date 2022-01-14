13f7bb086df6d91518840e82ad34069e69767563
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-01-14 19:15:09 +0530

    added expressjs

--- Node-Complete-Guide/app.js
+++ Node-Complete-Guide/app.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const express = require('express');
 
+const app = express();
 
-const routes = require('./routes');
+app.use((req,res,next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line
+});
 
-const server = http.createServer(routes);
-server.listen(3000);
+app.use((req,res,next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from express</h1>');
+})
+
+app.listen(3000);
