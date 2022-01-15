58c647812f6073d61978ea17dad43079b0d3884d
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-01-15 09:22:35 +0530

    added express routes

--- Node-Complete-Guide/app.js
+++ Node-Complete-Guide/app.js
@@ -3,25 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(adminRoutes);
+
+app.use(shopRoutes);
 
 
-app.use('/add-product',(req,res,next) => {
-    console.log('In another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.use('/product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/',(req,res,next) => {
-    console.log('This always runs!');
-    next();
-});
 
 
 
